51e3a4d5b7996fed05d035b364f4e2c952358813
Sebastian Gomez <ssgomez@trabajo.gob.ar>
2023-04-29 14:27:18 -0300

    Cambio en el diseño de las ventanas.

--- Components/Configuracion.js
+++ Components/Configuracion.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { Surface, Stack, Button } from "@react-native-material/core";
+import { Surface, Stack, Button, Text } from "@react-native-material/core";
 import { useNavigation } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Octicons } from '@expo/vector-icons';
+import { View } from 'react-native';
 
 function Configuracion() {
   const navigation = useNavigation();
+  let [fontsLoaded] = useFonts({
+    "BebasNeue": require('../assets/fonts/BebasNeue.ttf'),
+  })
+
   return (
     <Stack fill center spacing={4}>
       <Surface
@@ -16,7 +23,13 @@ function Configuracion() {
           height: 600,
         }}
       >
-        <Button title="Volver" onPress={() => navigation.navigate("Login")} style={{ width: '60%', height: 50, display: 'flex', justifyContent: 'center' }} />
+
+        <View style={{ margin: 10 }}>
+          <Octicons name="verified" size={90} color="#24CAE8" />
+        </View>
+        <Text style={{ fontFamily: 'BebasNeue', fontSize: 30, color: 'black' }}>Te registraste correctamente !</Text>
+        <Button title="Loguearme" onPress={() => navigation.navigate("Login")} style={{ width: '60%', height: 50, display: 'flex', marginTop: 40, justifyContent: 'center' }} />
+
       </Surface>
     </Stack>
   );
--- Components/Success.js
+++ Components/Success.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Surface, Stack, Button, Text } from "@react-native-material/core";
 import { useNavigation } from "@react-navigation/native";
 import { useFonts } from "expo-font";
+import { Octicons } from '@expo/vector-icons';
+import { View } from 'react-native';
 
-function Success() {
+const Success = () => {
   const navigation = useNavigation();
   let [fontsLoaded] = useFonts({
     "BebasNeue": require('../assets/fonts/BebasNeue.ttf'),
   })
-  
+
   return (
     <Stack fill center spacing={4}>
       <Surface
@@ -22,9 +24,13 @@ function Success() {
         }}
       >
 
-        <Text style={{ fontFamily: 'BebasNeue', fontSize: 30, color: 'green' }}>Te registraste correctamente !</Text>
+        <View style={{ margin: 10 }}>
+          <Octicons name="verified" size={90} color="#24CAE8" />
+        </View>
+        <Text style={{ fontFamily: 'BebasNeue', fontSize: 30, color: 'black' }}>Te registraste correctamente!</Text>
 
-        <Button title="Loguearme" onPress={() => navigation.navigate("Login")} style={{ width: '60%', height: 50, display: 'flex', marginTop: 30, justifyContent: 'center' }} />
+        <Button title="Loguearme" onPress={() => navigation.navigate("Login")} style={{ width: '60%', height: 50, display: 'flex', marginTop: 40, justifyContent: 'center' }} />
+
       </Surface>
     </Stack>
   );
