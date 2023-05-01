44c844013fa96739f48c4188ff2b921475284571
Sebastian Gomez <ssgomez@trabajo.gob.ar>
2023-05-01 19:25:17 -0300

    Cambios en el login y home.

--- Components/Home.js
+++ Components/Home.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Surface, Stack, Text } from "@react-native-material/core";
 import { useRoute } from "@react-navigation/native";
-import { useFonts } from "expo-font";
+import * as Font from 'expo-font';
 
 function Home() {
   const route = useRoute();
-  let [fontsLoaded] = useFonts({
-    "BebasNeue": require('../assets/fonts/BebasNeue.ttf'),
-  })
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  // Font propia
+  const loadFontAsync = async () => {
+    await Font.loadAsync({
+      'BebasNeue': require('../assets/fonts/BebasNeue.ttf'),
+    });
+    setFontLoaded(true);
+  }
+
+  useEffect(() => {
+    loadFontAsync();
+  }, []);
+
   return (
     <Stack fill center spacing={4}>
       <Surface elevation={4} category="medium" style={{ justifyContent: "center", alignItems: "center", width: 600, height: 600, }}>
 
-        <Text category="h4" style={{ width: '65%', textAlign: 'center', fontFamily: 'BebasNeue', fontSize: 30, marginTop: 20, maxWidth: "65%" }}>
+        <Text category="h4" style={{ width: '65%', textAlign: 'center', fontFamily: fontLoaded ? 'BebasNeue' : 'Arial', fontSize: 30, marginTop: 20, maxWidth: "65%" }}>
           Bienvenido, {route.params.email} !
         </Text>
 
--- Components/Login.js
+++ Components/Login.js
@@ -6,8 +6,8 @@ import axios from "axios";
 import { TextInput } from 'react-native-paper';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import styles from '../Utils/Styles';
+import * as Font from 'expo-font';
 
 function Login() {
   const navigation = useNavigation();
@@ -15,11 +15,19 @@ function Login() {
   const [modalVisible, setModalVisible] = useState(false);
   const [email, setEmail] = useState('');
   const [clave, setClave] = useState('');
+  const [fontLoaded, setFontLoaded] = useState(false);
 
   // Font propia
-  let [fontsLoaded] = useFonts({
-    "BebasNeue": require('../assets/fonts/BebasNeue.ttf'),
-  })
+  const loadFontAsync = async () => {
+    await Font.loadAsync({
+      'BebasNeue': require('../assets/fonts/BebasNeue.ttf'),
+    });
+    setFontLoaded(true);
+  }
+
+  useEffect(() => {
+    loadFontAsync();
+  }, []);
 
   // Funcion para las validaciones
   const validarLogin = () => {
@@ -50,7 +58,7 @@ function Login() {
     const errores = validarLogin();
     if (errores.length === 0) {
       try {
-        const response = await axios.post('http://192.168.1.100:8282/auth/login', {
+        const response = await axios.post('http://localhost:8282/auth/login', {
           email: email,
           clave: clave
         });
@@ -85,7 +93,9 @@ function Login() {
         <TextInput label="Contraseña" mode="outlined" placeholder="Contraseña" value={clave} onChangeText={text => setClave(text)} maxLength={15} secureTextEntry right={<TextInput.Affix text="/15" />} style={{ width: '80%', maxWidth: "60%", minWidth: "30%", height: 50, marginBottom: 50, display: 'flex', justifyContent: 'center' }} />
 
         <Button title="Ingresar" onPress={async () => await handleLogin(email, clave)} style={{ backgroundColor: '#24CAE8', width: '80%', textAlign: 'center', maxWidth: "60%", minWidth: "30%", height: 50, marginBottom: 15, display: 'flex', justifyContent: 'center' }} />
-        <Button title="Registrarme" onPress={() => navigation.navigate("Registro")} style={{ backgroundColor: '#24CAE8', width: '80%', textAlign: 'center', maxWidth: "60%", minWidth: "30%", height: 50, display: 'flex', justifyContent: 'center' }} />
+        <Button title="Registrarme" onPress={() => navigation.navigate("Registro")} style={{ width: '80%', textAlign: 'center', maxWidth: "60%", minWidth: "30%", height: 50, display: 'flex', justifyContent: 'center' }} />
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end', width: '60%' }}>
+          <Text category="h4" onPress={() => navigation.navigate("Recupero")} style={{ fontFamily: fontLoaded ? 'BebasNeue' : 'Arial', marginTop: 20, flex: 1, textAlign: 'right', color: '#696969' }}>Olvidaste tu contraseña ?</Text></View>
 
         <View style={styles.centeredView}>
           <Modal visible={modalVisible} transparent={true} onRequestClose={() => setModalVisible(false)} animationType="slide">
@@ -93,7 +103,7 @@ function Login() {
               {errores.map((error, index) => (
                 <View key={index} style={{ flexDirection: 'row', alignItems: 'center', height: 25, marginBottom: 20, justifyContent: 'center' }}>
                   <Feather name="x-octagon" size={22} color="#900" />
-                  <Text style={{ fontFamily: 'BebasNeue', fontSize: 18, color: 'black', marginTop: 4, marginLeft: 10 }}>{error}</Text>
+                  <Text style={{ fontFamily: fontLoaded ? 'BebasNeue' : 'Arial', fontSize: 18, color: 'black', marginTop: 4, marginLeft: 10 }}>{error}</Text>
                 </View>
               ))}
               <View style={{ width: '100%', alignItems: 'center' }}>
