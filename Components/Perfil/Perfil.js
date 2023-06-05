ebab0caf08738bcd355d314013ae884e03e4d7ff
Sebastian Gomez <ssgomez@trabajo.gob.ar>
2023-06-05 20:43:53 -0300

    agregando funcion para obtener datos de usuario

--- Components/Perfil/Perfil.js
+++ Components/Perfil/Perfil.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { Surface, Stack, Text } from "@react-native-material/core";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { View, Modal, ScrollView, Switch, TouchableWithoutFeedback } from 'react-native';
+import { ScrollView } from 'react-native';
 import * as Font from 'expo-font';
 import styles from '../Utils/Styles';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 function Perfil() {
     const navigation = useNavigation();
     const route = useRoute();
     const [fontLoaded, setFontLoaded] = useState(false);
+    const [user, setUser] = useState(null);
 
     // Font propia
     const loadFontAsync = async () => {
@@ -21,43 +21,47 @@ function Perfil() {
     }
 
     useEffect(() => {
+        const getUserByEmail = async () => {
+            const email = route.params.email; // Replace with the email you have
+            console.log(email);
+            try {
+                const response = await axios.get(`http://localhost:8282/user/email/${email}`);
+                setUser(response.data);
+                console.log(user);
+            } catch (error) {
+                if (error.response) {
+                    // Request was made and server responded with a status code
+                    console.log("Error:" + error.response.data);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    console.log("Error, No response received from the server");
+                } else {
+                    // Something happened in setting up the request that triggered an error
+                    console.log("Error, An unexpected error occurred");
+                }
+            }
+        };
+
+        getUserByEmail();
         loadFontAsync();
     }, []);
 
+    return (
 
-    // Funcion Perfil
-    async function handlePerfil(email) {
-        try {
-            const response = await axios.post('http://localhost:8282/user/email', {
-                email: route.params.email
-            });
-            AsyncStorage.setItem("token", token);
-            navigation.navigate('Perfil', { email: email });
-        } catch (error) {
-            if (error.response && error.response.data === "Email o contraseña incorrectos") {
-                setErrores(["Email o contraseña incorrectos."]);
-                setModalVisible(true);
-            }
-            console.error(error);
-        }
-    }
+        <ScrollView>
+            <Stack fill center spacing={4}>
+                <Surface elevation={4} category="medium" style={styles.surfaceTitle}>
+                    <Text style={styles.titulo}>
+                        Bienvenido, {route.params.email} !
+                    </Text>
+                </Surface>
 
-return (
+                <Surface elevation={4} category="medium" style={styles.surfaceViajes}>
+                </Surface>
+            </Stack>
+        </ScrollView>
 
-    <ScrollView>
-        <Stack fill center spacing={4}>
-            <Surface elevation={4} category="medium" style={styles.surfaceTitle}>
-                <Text style={styles.titulo}>
-                    Bienvenido, {route.params.email} !
-                </Text>
-            </Surface>
-
-            <Surface elevation={4} category="medium" style={styles.surfaceViajes}>
-            </Surface>
-        </Stack>
-    </ScrollView>
-
-);
+    );
 }
 
 export default Perfil;
